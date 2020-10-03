a23ed50c68f4be5a380817065348d8675aa7a702
celikozkan <ozkan44celik@gmail.com>
2020-10-03 21:04:43 +0300

    CORS header 'Access-Control-Allow-Origin' added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, url_for
 from bs4 import BeautifulSoup
 import requests, json, xmltodict
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/playlist/', methods=['GET'])
 def playlist():
